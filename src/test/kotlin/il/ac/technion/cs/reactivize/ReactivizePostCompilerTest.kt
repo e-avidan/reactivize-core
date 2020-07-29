b3f4c71e8197d2d2557b717d8905e9ca17bbe08d
Matan Peled <mip@cs.technion.ac.il>
2020-07-29 13:22:52 +0300

    Initial (trivial) test.

---
+++ src/test/kotlin/il/ac/technion/cs/reactivize/ReactivizePostCompilerTest.kt
@@ -0,0 +1,10 @@
+package il.ac.technion.cs.reactivize
+
+import org.junit.jupiter.api.Test
+
+class ReactivizePostCompilerTest {
+    @Test
+    fun sanity() {
+        val compiler = ReactivizePostCompiler()
+    }
+}
