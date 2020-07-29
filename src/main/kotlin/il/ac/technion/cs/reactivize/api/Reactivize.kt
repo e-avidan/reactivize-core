cd64645b2282d4508cb528fb9c1a686ac5a6c1cd
Matan Peled <mip@cs.technion.ac.il>
2020-07-29 14:44:34 +0300

    Add annotation API

---
+++ src/main/kotlin/il/ac/technion/cs/reactivize/api/Reactivize.kt
@@ -0,0 +1,4 @@
+package il.ac.technion.cs.reactivize.api
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+annotation class Reactivize
